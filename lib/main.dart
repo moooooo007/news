cc2ceb00b73761c6985c86949ec47549430eb917
Mohammed Safaa <ictp.m2010@gmail.com>
2022-09-13 20:04:38 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Newas());
+}
+class Newas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute:  "",
+      routes: {
+
+      },
+    );
+  }
+}
